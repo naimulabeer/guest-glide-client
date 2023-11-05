b0391f211c7b88a66b927928f2744f24219296db
naimulabeer <naimul.h.abeer@gmail.com>
2023-11-06 03:58:09 +0600

    Error and about page created

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,33 +1,28 @@
-import { useRouteError, Link, useNavigate } from "react-router-dom"
-import "./error.styles.css"
+import { Link, useNavigate } from "react-router-dom";
+import "./error.styles.css";
 
-import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
 function Error() {
-  const error = useRouteError();
   const navigate = useNavigate();
 
   return (
-    <div className="error">
-      <h1>Uh oh! We’ve got a problem.</h1>
-      <p>{error.message || error.statusText}</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img src="/error.webp" alt="Error" className="max-w-md w-full h-auto" />
+      <h1 className="text-4xl font-bold mt-4">
+        Sorry, we could not find the page you are looking for.
+      </h1>
       <div className="flex">
-        <button
-          className="btn"
-          onClick={() => navigate(-1)}
-        >
+        <button className="btn" onClick={() => navigate(-1)}>
           <ArrowUturnLeftIcon width={20} />
           <span>Go Back</span>
         </button>
-        <Link
-          to="/"
-          className="btn btn--dark"
-        >
+        <Link to="/" className="btn btn--dark">
           <HomeIcon width={20} />
           <span>Go home</span>
         </Link>
       </div>
     </div>
-  )
+  );
 }
-export default Error
+export default Error;
--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import SwiperImage from "../../components/Swiper/Swiper";
 import { GoPeople } from "react-icons/go";
 
@@ -6,8 +6,15 @@ function RoomDetails() {
   const roomsDetails = useLoaderData();
 
   // eslint-disable-next-line no-unused-vars
-  const { _id, name, images, price_per_night, room_size, room_description } =
-    roomsDetails;
+  const {
+    _id,
+    name,
+    images,
+    price_per_night,
+    room_size,
+    room_description,
+    available_seats,
+  } = roomsDetails;
   return (
     <div className="mt-10 mb-10">
       <SwiperImage images={images} />
@@ -21,16 +28,20 @@ function RoomDetails() {
           <p className="text-2xl border-b-2  border-b-blue-500 w-fit uppercase">
             Description
           </p>
-          <p className="">{room_description}</p>
+          <p className="text-lg text-slate-700">{room_description}</p>
+          <p className="text-lg">Availablity: {available_seats}</p>
         </div>
         <div className="w-full lg:w-1/3 px-10 py-4 mt-10 flex flex-col gap-2 lg:ml-10">
           <span className="text-lg font-bold">Price:</span>
           <span className="text-lg">
             <span className="font-bold">${price_per_night}</span>/night
           </span>
-          <button className=" lg:w-4/5 w-1/2 px-6 py-2 font-bold uppercase border bg-amber-400 hover:bg-emerald-500">
+          <Link
+            to={`/room/${_id}/bookingPage`}
+            className=" lg:w-4/5 w-1/2 px-6 text-center py-2 font-bold uppercase border bg-amber-400 hover:bg-emerald-500"
+          >
             Book This Room
-          </button>
+          </Link>
         </div>
       </div>
     </div>
